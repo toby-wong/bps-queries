73531244a1e90d24a4df0b3299c109e6bd8c5d7a
Toby Wong <57238401+toby-wong@users.noreply.github.com>
2021-09-08 17:05:25 +1000

    Update Duplicated patients by dob, name.sql

--- Duplicated patients by dob, name.sql
+++ Duplicated patients by dob, name.sql
@@ -1,8 +1,8 @@
 SELECT *
-FROM BPS_Patients
-inner join BPS_Patients as B1 on not b1.internalid = BPS_Patients.internalid
- and BPS_Patients.dob = b1.dob
- and BPS_Patients.surname = b1.surname
- and BPS_Patients.firstname = b1.firstname
-WHERE BPS_Patients.StatusText = 'Active' and b1.statustext = 'Active'
-ORDER BY BPS_Patients.surname, BPS_Patients.firstname
+FROM BPS_Patients p
+INNER JOIN BPS_Patients AS p1 ON NOT p1.internalid = p.internalid
+ AND p.dob = p1.dob
+ AND p.surname = p1.surname
+ AND p.firstname = p1.firstname
+WHERE p.StatusText = 'Active' AND p1.statustext = 'Active'
+ORDER BY p.surname, p.firstname
